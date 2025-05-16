7809ecd4dc279b5546bf970d8152d6e458a983f3
Vinicius <vinicius.lopes.pimentel@gmail.com>
2025-05-16 12:56:03 -0300

    feat: enable transformation in global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { setupSwagger } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const configService = app.get(ConfigService);
   const parsed = envSchema.safeParse(process.env);
